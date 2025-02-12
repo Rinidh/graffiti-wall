fd78ced1d8e708d5fad0feeea4d64fae6c4ea718
Rinidh <rinidhdhokia@gmail.com>
2025-02-12 22:47:56 +0300

    Refactor: prevent replcaement of boards for each tool selection; use switch-case in mousemove events to draw various shapes according to rightToolBar.dataset.activeTool. Rename 'deacticateAll()' to 'removeActiveClassAll()' to better understand its purpose

--- index.js
+++ index.js
@@ -16,15 +16,14 @@ currentBoard.style.backgroundColor = "black"
 
 toolIcons.forEach((icon, idx) => {
   icon.addEventListener("click", () => {
-    deactivateAll()
+    removeActiveClassAll()
 
     rightToolBar.dataset.activeTool = icon.dataset.toolName
     icon.classList.add("active")
-    handleToolChange()
   })
 })
 
-function deactivateAll() {
+function removeActiveClassAll() {
   toolIcons.forEach((elem) => {
     elem.classList.remove("active")
     rightToolBar.dataset.activeTool = "none"
@@ -32,114 +31,62 @@ function deactivateAll() {
 
 }
 
-function handleToolChange() {
+
+currentBoard.addEventListener("mousedown", (e) => {
+  startX = e.offsetX
+  startY = e.offsetY
+  isDrawing = true
+})
+
+currentBoard.addEventListener("mouseup", () => {
+  isDrawing = false
+})
+
+currentBoard.addEventListener("mousemove", handleMouseMove)
+
+function handleMouseMove(mouseEvent) {
+  if (!isDrawing) return
+
   switch (rightToolBar.dataset.activeTool) {
     case "line":
-      triggerLineTool()
+      drawLine(mouseEvent)
       break;
 
     case "circle":
-      triggerCircleTool()
+      drawCircle(mouseEvent)
       break;
 
     case "rectangle":
-      triggerRectangleTool()
+      drawRectangle(mouseEvent)
       break;
 
   }
-}
-
-function triggerLineTool() {
-  removeAllEventListeners()
-
-  currentBoard.addEventListener("mousedown", (e) => {
-    startX = e.offsetX
-    startY = e.offsetY
-    isDrawing = true
-  })
-
-  currentBoard.addEventListener("mousemove", (e) => {
-    if (!isDrawing) return;
-
-    ctx.clearRect(0, 0, currentBoard.width, currentBoard.height) //canvas elems have .height & .width, more accurate than .clientHeight & .clientWidth global props
-    ctx.beginPath()
-    ctx.moveTo(startX, startY)
-    ctx.lineTo(e.offsetX, e.offsetY)
-    ctx.lineWidth = 2
-    ctx.stroke()
-
-  })
-
-  currentBoard.addEventListener("mouseup", () => {
-    isDrawing = false
-  })
-}
-
-function triggerCircleTool() {
-  removeAllEventListeners()
-
-  currentBoard.addEventListener("mousedown", (e) => {
-    startX = e.offsetX
-    startY = e.offsetY
-    isDrawing = true
-  })
-
-  currentBoard.addEventListener("mousemove", (e) => {
-    if (!isDrawing) return
-
-    ctx.clearRect(0, 0, currentBoard.width, currentBoard.height)
-    ctx.beginPath()
-    const dx = Math.abs(e.offsetX - startX)
-    const dy = Math.abs(e.offsetY - startY)
-    ctx.ellipse(startX, startY, dx, dy, 0, 0, Math.PI * 2, false)
-    ctx.stroke()
-
-  })
-
-  currentBoard.addEventListener("mouseup", () => {
-    isDrawing = false
-  })
 
 }
 
-function triggerRectangleTool() {
-  removeAllEventListeners()
-
-  currentBoard.addEventListener("mousedown", (e) => {
-    startX = e.offsetX
-    startY = e.offsetY
-    isDrawing = true
-  })
-
-  currentBoard.addEventListener("mousemove", (e) => {
-    if (!isDrawing) return
-
-    ctx.clearRect(0, 0, currentBoard.width, currentBoard.height)
-    ctx.beginPath()
-    const dx = e.offsetX - startX
-    const dy = e.offsetY - startY
-    ctx.rect(startX, startY, dx, dy)
-    ctx.stroke()
-
-  })
-
-  currentBoard.addEventListener("mouseup", () => {
-    isDrawing = false
-  })
-
+function drawLine(e) {
+  ctx.clearRect(0, 0, currentBoard.width, currentBoard.height) //canvas elems have .height & .width, more accurate than .clientHeight & .clientWidth global props
+  ctx.beginPath()
+  ctx.moveTo(startX, startY)
+  ctx.lineTo(e.offsetX, e.offsetY)
+  ctx.lineWidth = 2
+  ctx.stroke()
 }
 
-function removeAllEventListeners() {
-  const newBoard = currentBoard.cloneNode(true)
-  currentBoard.parentNode.replaceChild(newBoard, currentBoard)
-  currentBoard = newBoard
+function drawCircle(e) {
+  ctx.clearRect(0, 0, currentBoard.width, currentBoard.height)
+  ctx.beginPath()
+  const dx = Math.abs(e.offsetX - startX)
+  const dy = Math.abs(e.offsetY - startY)
+  ctx.ellipse(startX, startY, dx, dy, 0, 0, Math.PI * 2, false)
+  ctx.stroke()
+}
 
-  currentBoard.height = currentBoard.clientHeight
-  currentBoard.width = currentBoard.clientWidth
-  ctx = currentBoard.getContext("2d") //reinitialize context to use context of the new board
-
-  ///
-  ctx.strokeStyle = "red"
-  currentBoard.style.backgroundColor = "black"
-
+function drawRectangle(e) {
+  ctx.clearRect(0, 0, currentBoard.width, currentBoard.height)
+  ctx.beginPath()
+  const dx = e.offsetX - startX
+  const dy = e.offsetY - startY
+  ctx.rect(startX, startY, dx, dy)
+  ctx.stroke()
 }
