81b59e22f1932bf638fd1a6ed5e6421bbac485fc
Rinidh <rinidhdhokia@gmail.com>
2025-02-12 21:45:17 +0300

    Enable tool selection and add event listeners accordingly. Change event listeners on every tool-change. Discovery: always explicitly set canvas height & width in JS to match CSS set height & width

--- index.js
+++ index.js
@@ -1,52 +1,93 @@
 const toolIcons = document.querySelectorAll(".tool-bar i")
 const rightToolBar = document.querySelector(".tool-bar.right-bar")
-const board = document.querySelector(".board")
+const initialBoard = document.querySelector(".board")
 
-const ctx = board.getContext("2d")
+let currentBoard = initialBoard
+currentBoard.height = currentBoard.clientHeight //without explicitly setting these two to match CSS width (clientWidth) & CSS height (clientHeight), lines are drawn at default canvas 300 X 150 px size, appearing at weird postions on this big canvas
+currentBoard.width = currentBoard.clientWidth
 
-board.width = board.clientWidth
-board.height = board.clientHeight
+let ctx = currentBoard.getContext("2d")
 let startX, startY
 let isDrawing = false
 
-board.addEventListener("mousedown", (e) => {
-  startX = e.offsetX
-  startY = e.offsetY
-  isDrawing = true
-})
-
-board.addEventListener("mousemove", (e) => {
-  // console.log(isDrawing)
-  if (!isDrawing) return;
-
-  ctx.clearRect(0, 0, board.width, board.height)
-
-  ctx.beginPath()
-  ctx.moveTo(startX, startY)
-  ctx.lineTo(e.offsetX, e.offsetY)
-  ctx.lineWidth = 2
-  ctx.stroke()
-
-})
-
-board.addEventListener("mouseup", () => {
-  isDrawing = false
-})
-
-
-//Tool selection
-toolIcons.forEach((icon, idx, toolIcons) => {
+toolIcons.forEach((icon, idx) => {
   icon.addEventListener("click", () => {
-    deactivateAll(toolIcons)
+    deactivateAll()
 
     rightToolBar.dataset.activeTool = icon.dataset.toolName
     icon.classList.add("active")
+    handleToolChange()
   })
 })
 
-function deactivateAll(nodeList) {
-  nodeList.forEach((elem) => {
+function deactivateAll() {
+  toolIcons.forEach((elem) => {
     elem.classList.remove("active")
+    rightToolBar.dataset.activeTool = "none"
   })
+
+}
+
+function handleToolChange() {
+  switch (rightToolBar.dataset.activeTool) {
+    case "line":
+      triggerLineTool()
+      break;
+
+    case "circle":
+      triggerCircleTool()
+      break;
+
+    case "rectangle":
+      triggerRectangleTool()
+      break;
+
+  }
+}
+
+function triggerLineTool() {
+  removeAllEventListeners()
+
+  currentBoard.addEventListener("mousedown", (e) => {
+    startX = e.offsetX
+    startY = e.offsetY
+    isDrawing = true
+  })
+
+  currentBoard.addEventListener("mousemove", (e) => {
+    // console.log(isDrawing)
+    if (!isDrawing) return;
+
+    ctx.clearRect(0, 0, currentBoard.width, currentBoard.height) //canvas elems have .height & .width, more accurate than .clientHeight & .clientWidth global props
+
+    ctx.beginPath()
+    ctx.moveTo(startX, startY)
+    ctx.lineTo(e.offsetX, e.offsetY)
+    ctx.lineWidth = 2
+    ctx.stroke()
+
+  })
+
+  currentBoard.addEventListener("mouseup", () => {
+    isDrawing = false
+  })
+}
+
+function triggerCircleTool() {
+
+}
+
+function triggerRectangleTool() {
+
+}
+
+function removeAllEventListeners() {
+  const newBoard = currentBoard.cloneNode(true)
+  currentBoard.parentNode.replaceChild(newBoard, currentBoard)
+  currentBoard = newBoard
+
+  currentBoard.height = currentBoard.clientHeight
+  currentBoard.width = currentBoard.clientWidth
+  ctx = currentBoard.getContext("2d") //reinitialize context to use context of the new board
 
 }
